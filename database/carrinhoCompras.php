c48911844a195db6b36f68dd90248e26272755f7
luizfvpereira <luizfvpereira@gmail.com>
2015-05-20 19:46:14 +0100

    shop-cart

--- database/carrinhoCompras.php
+++ database/carrinhoCompras.php
@@ -4,7 +4,7 @@ function searchItems() {
         $stmt = $conn->prepare("            
 			SELECT itemEncomenda.quantidade, produto.nome, (itemEncomenda.quantidade*produto.preco) as total
 			FROM itemEncomenda, produto
-			WHERE produto.id = itemEncomenda.idProduto AND itemEncomenda.idCarrinho = (SELECT id FROM carrinhoCompras WHERE idUser = 1 AND estado = FALSE;");
+			WHERE produto.id = itemEncomenda.idProduto AND itemEncomenda.idCarrinho = (SELECT id FROM carrinhoCompras WHERE idUser = 1 AND estado = FALSE");
         $stmt->execute();
         return $stmt->fetchAll();
     }
