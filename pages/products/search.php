116f2ae7a63c6eabb04328696f0cf2d30b06bc98
luizfvpereira <luizfvpereira@gmail.com>
2015-05-21 17:48:15 +0100

    repus a parte das imagens, search

--- pages/products/search.php
+++ pages/products/search.php
@@ -4,19 +4,16 @@
 
     $searchResult = getSearchResult($_GET['pesquisa']);
 
-    // foreach ($searchResult as $key => $product)
-    // {
-        // unset($photo);
-        // if(file_exists($BASE_DIR.'images/produtos/'.$product['id'].'.png'))
-            // $photo = 'images/produtos/'.$product['id'].'.png';
-        // if(file_exists($BASE_DIR.'images/produtos/'.$product['id'].'.jpg'))
-          // $photo = 'images/produtos/'.$product['id'].'.jpg';
-        // if (!$photo) $photo = 'images/produtos/default.png';
-        // $recentProducts[$key]['photo'] = $photo;
-    // }
-	foreach ($searchResult as $a){
-		$a['caminhoImagem'] = 'images/produtos/default.png';
-	}
+    foreach ($searchResult as $key => $product)
+    {
+        unset($photo);
+        if(file_exists($BASE_DIR.'images/produtos/'.$product['id'].'.png'))
+            $photo = 'images/produtos/'.$product['id'].'.png';
+        if(file_exists($BASE_DIR.'images/produtos/'.$product['id'].'.jpg'))
+          $photo = 'images/produtos/'.$product['id'].'.jpg';
+        if (!$photo) $photo = 'images/produtos/default.png';
+        $recentProducts[$key]['photo'] = $photo;
+    }
 	//acrescentei este codigo
 	if (!empty($_GET['idP'])) {
 	  addItem(1,$_GET['idP'], 1); //o primeiro 1 sera substituido pela quantidade e o segundo 1 sera substituido pelo id do utilizador com sessão aberta
