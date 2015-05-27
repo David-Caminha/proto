5117e38063d714ee4a834aa71d10b2a8fb7bbee9
David-Caminha <david.caminha94@gmail.com>
2015-05-27 13:13:52 +0100

    erro no nome da funcao

--- pages/users/perfil.php
+++ pages/users/perfil.php
@@ -2,7 +2,7 @@
     include_once('../../config/init.php');
     include_once($BASE_DIR .'database/users.php');
 
-    $type = getType($_SESSION['username']);
+    $type = getTipo($_SESSION['username']);
     if($type == 2)
         header('Location: '.$BASE_URL. 'pages/users/pagDono.php');
     else
