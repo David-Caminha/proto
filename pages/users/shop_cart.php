1c4bacabee6c6eefd706aeeac9a346be773f7176
luizfvpereira <luizfvpereira@gmail.com>
2015-05-28 16:58:55 +0100

    shop-cart

--- pages/users/shop_cart.php
+++ pages/users/shop_cart.php
@@ -4,6 +4,7 @@
   $itemEncomenda = searchItems($_SESSION['username']);
   if (!empty($_GET['idC']) && !empty($_GET['idP'])) {
 	  removeItem($_GET['idC'],$_GET['idP'], $_SESSION['username']);
+	  header( "users/shop_cart.php" );
   }
   $smarty->assign('Result', $itemEncomenda);
   $smarty->display('users/shop_cart.tpl');
