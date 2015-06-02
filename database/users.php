a89e3ccc1b2eef3ff58b8a68048f7a0f3a42a93e
luizfvpereira <luizfvpereira@gmail.com>
2015-06-02 13:36:23 +0100

    perfil

--- database/users.php
+++ database/users.php
@@ -106,12 +106,18 @@
 	function updateInfoUser($u_nome, $u_email, $u_telemovel, $u_date, $m_rua, $m_cp2, $c_nome, $u_name) {
 		global $conn;
 		$stmt = $conn->prepare("
-			UPDATE utilizador, morada SET utilizador.nome = ?, utilizador.email = ?, utilizador.telemovel = ?,
-			utilizador.dataNascimento = ?, morada.rua = ?, morada.CP2 = ?, morada.idCidade = (SELECT id FROM cidade WHERE cidade.nome = ?)
-			WHERE utilizador.id = morada.idUser AND
-			utilizador.id = (SELECT id FROM utilizador WHERE username = ?)
+			UPDATE utilizador SET utilizador.nome = ?, utilizador.email = ?, utilizador.telemovel = ?,
+			utilizador.dataNascimento = ?
+			WHERE utilizador.id = (SELECT id FROM utilizador WHERE username = ?)
 		");
-		$stmt->execute(array($u_nome, $u_email, $u_telemovel, $u_date, $m_rua, $m_cp2, $c_nome, $u_name));
+		$stmt->execute(array($u_nome, $u_email, $u_telemovel, $u_date, $u_name));
+		
+		$stmt2 = $conn->prepare("
+			UPDATE morada SET morada.rua = ?, morada.CP2 = ?, morada.idCidade = (SELECT id FROM cidade WHERE cidade.nome = ?)
+			WHERE morada.idUser = (SELECT id FROM utilizador WHERE username = ?)
+		");
+		$stmt2->execute(array($m_rua, $m_cp2, $c_nome, $u_name));
+		
 		return true;
 	}
 	
