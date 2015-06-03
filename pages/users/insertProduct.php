e819d5b5968fa8253fb624c173856197fb364421
luizfvpereira <luizfvpereira@gmail.com>
2015-06-03 16:36:09 +0100

    insertProduct

--- pages/users/insertProduct.php
+++ pages/users/insertProduct.php
@@ -2,4 +2,48 @@
     include_once('../../config/init.php');
     include_once($BASE_DIR .'database/products.php');
     
+	if($_GET['name'] && $_GET['price'] && $_GET['description'] && $_GET['stock'] && $_GET['fileToUpload'] && $_GET['technic_details'] && $_GET['brand'] && $_GET['type'])
+	{
+		$target_file = $BASE_DIR .'\images'. basename($_FILES["fileToUpload"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+		// Check if image file is a actual image or fake image
+		if(isset($_POST["submit"])) {
+			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			if($check !== false) {
+				$uploadOk = 1;
+			} else {
+				echo "<script type='text/javascript'>alert('File is not an image.');</script>";
+				$uploadOk = 0;
+			}
+		}
+		// Check if file already exists
+		if (file_exists($target_file)) {
+			echo "<script type='text/javascript'>alert('Sorry, file already exists.');</script>";
+			$uploadOk = 0;
+		}
+		// Check file size
+		if ($_FILES["fileToUpload"]["size"] > 500000) {
+			echo "<script type='text/javascript'>alert('Sorry, your file is too large.');</script>";
+			$uploadOk = 0;
+		}
+		// Allow certain file formats
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
+			echo "<script type='text/javascript'>alert('Sorry, only JPG, JPEG & PNG files are allowed.');</script>";
+			$uploadOk = 0;
+		}
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+			echo "<script type='text/javascript'>alert('Sorry, your file was not uploaded.');</script>";
+		// if everything is ok, try to upload file
+		} else {
+			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			} else {
+				echo "<script type='text/javascript'>alert('Sorry, there was an error uploading your file.');</script>";
+			}
+		}
+	} else {
+		echo "<script type='text/javascript'>alert('Preencha todos os campos por favor.');</script>";
+	}
+	
 	$smarty->display('users/insertProduct.tpl');
--- templates/users/insertProduct.tpl
+++ templates/users/insertProduct.tpl
@@ -4,21 +4,21 @@
 	<div id="product_wrapper">
 		<form action="{$BASE_URL}pages/users/insertProduct.php" method="post" enctype="multipart/form-data">
 			Nome do Produto:<input type="text" name="name" /><br/>
-			Preço:<input type="number" name="price" min="0" step="0.01"/><br/>
-			Descrição:<textarea rows="10" cols="50" name="description" placeholder="Escreva aqui uma descrição do produto..."></textarea><br/>
+			Preço:<input type="number" name="price" min="0.01" step="0.01"/><br/>
+			Descrição:<textarea rows="6" cols="50" name="description" placeholder="Escreva aqui uma descrição do produto..."></textarea><br/>
 			Stock Inicial:<input type="number" name="stock" /><br/>
 			Imagem:
 			<input type="file" name="fileToUpload" id="fileToUpload">
-			Ficha Técnica:<textarea rows="10" cols="50" name="technic_details" placeholder="Escreva aqui os tópicos da ficha técnica..."></textarea><br/>
-			Marca:<input type="text" name="" /><br/>
+			Ficha Técnica:<textarea rows="6" cols="50" name="technic_details" placeholder="Escreva aqui os tópicos da ficha técnica..."></textarea><br/>
+			Marca:<input type="text" name="brand" /><br/>
 			Tipo de Produto:
-			<select>
+			<select name="type">
 				<option value="1" selected>Laptop</option>
 				<option value="2">Desktop</option>
 				<option value="3">Periférico</option>
 				<option value="4">Cabo</option>
 			</select><br/>
-			<button>Inserir Produto</button>
+			<input type="submit" name="submit" value="Inserir Produto">
 		</form>
 	</div>
 {else}
