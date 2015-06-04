43c2a1b749e692834fc72783c40a207a406ed4c8
luizfvpereira <luizfvpereira@gmail.com>
2015-06-04 15:19:39 +0100

    editProduct

--- pages/users/editProduct.php
+++ pages/users/editProduct.php
@@ -4,7 +4,9 @@
 	
 	$p_id = $_GET['idProd'];
 	$info = populate_product_info($p_id);
+	$brands = getBrands();
 	
-	$smarty->assign('product', $info[0]);
+	$smarty->assign('p', $info[0]);
+	$smarty->assign('brands', $brands);
 	$smarty->display('users/editProduct.tpl');
 ?>
