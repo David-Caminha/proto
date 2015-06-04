e2e120c3d56dc8484d3d9e112f069df585ceb62d
luizfvpereira <luizfvpereira@gmail.com>
2015-06-04 17:02:19 +0100

    editFornecedor

--- pages/users/perfil.php
+++ pages/users/perfil.php
@@ -12,9 +12,11 @@
 		
 		if($_POST['nome'] && $_POST['email'] && $_POST['telemovel'] && $_POST['data_nascimento'] && $_POST['rua'] && $_POST['CP2'] && $_POST['cidade'] ) {
 			updateInfoUser($_POST['nome'], $_POST['email'], $_POST['telemovel'], $_POST['data_nascimento'], $_POST['rua'], $_POST['CP2'], $_POST['cidade'], $_SESSION['username']);
+			header('Location: '.$BASE_URL. 'pages/users/perfil.php');
 		}
 		if($_POST['password']==$_POST['confirm_password'] && $_POST['password']!='' && $_POST['password']!='' && $_POST['password'] && $_POST['confirm_password']) {
 			updateUserPassword($_POST['old_password'], $_POST['password'], $_SESSION['username']);
+			header('Location: '.$BASE_URL. 'pages/users/perfil.php');
 		}
 		
 		$smarty->assign('info', $info);
@@ -24,9 +26,11 @@
 		$info = getSupplierInfo($_SESSION['fornecedor']);
 		if($_POST['email'] && $_POST['telemovel'] && $_POST['responsavel'] ) {
 			updateSupplierInfo($_POST['email'], $_POST['telemovel'], $_POST['responsavel'], $_SESSION['fornecedor']);
-		}
+			header('Location: '.$BASE_URL. 'pages/users/perfil.php');
+}
 		if($_POST['password']==$_POST['confirm_password'] && $_POST['password']!='' && $_POST['password']!='' && $_POST['password'] && $_POST['confirm_password']) {
 			updateSupplierPassword($_POST['old_password'], $_POST['password'], $_SESSION['fornecedor']);
+			header('Location: '.$BASE_URL. 'pages/users/perfil.php');
 		}
 		$smarty->assign('info', $info);
 		$smarty->display('users/perfil_frn.tpl');
