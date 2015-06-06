42292f25db9f6458d01d024b203e38a5b144b113
luizfvpereira <luizfvpereira@gmail.com>
2015-06-06 12:59:53 +0100

    checkout

--- pages/users/checkout.php
+++ pages/users/checkout.php
@@ -5,7 +5,7 @@
 	if($_GET['confirm'])
 	{
 		checkout($_SESSION['username']);
-		// header("Location: $BASE_URL" . 'pages/users/shop_cart.php');
+		header("Location: $BASE_URL" . 'pages/users/shop_cart.php');
 	}
 	
 	$smarty->display('users/checkout.tpl');
