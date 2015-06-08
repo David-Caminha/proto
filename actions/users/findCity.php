9f37e179b4b2f4540d8e53d1a1dcb31fd5ad7869
AnaMoura <anacrmoura@gmail.com>
2015-06-08 03:10:01 +0100

    fid city

--- actions/users/findCity.php
+++ actions/users/findCity.php
@@ -3,12 +3,13 @@
     include_once($BASE_DIR .'database/users.php');  
     $country = $_GET['cidade'];
 
-    $result = cidadesPertencentes($country);
+    $cidades = cidadesPertencentes($country);
+    $smarty->assign('cidades', $cidades);
 ?>
 
 <select name="city">
 <option>Select City</option>
-<?php foreach($result as $row) { ?>
-<option value=<?php echo $row['id']?>><?php echo $row['nome']?></option>
+{foreach $cidades as $cidade}
+<option value="{$cidade.id}">{$cidade.nome}</option>
 <?php } ?>
 </select>
