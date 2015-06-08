2e49cbd144664af26847cfd0ebe84581f341df99
luizfvpereira <luizfvpereira@gmail.com>
2015-06-08 11:50:39 +0100

    search

--- pages/products/search.php
+++ pages/products/search.php
@@ -9,13 +9,13 @@
         unset($photo);
         if(file_exists($BASE_DIR.'images/produtos/'.$product['id'].'.png') && $product['caminhoimagem'] != 'images/produtos/'.$product['id'].'.png')
         {
-            $photo = 'images/produtos/'.$product['caminhoimagem'].'.png';
+            $photo = 'images/produtos/'.$product['id'].'.png';
             updatePath($product['id'], $photo);
             $searchResult = getSearchResult($_GET['pesquisa'], $_GET['method_receiver']);
         }
         else if(file_exists($BASE_DIR.'images/produtos/'.$product['id'].'.jpg') && $product['caminhoimagem'] != 'images/produtos/'.$product['id'].'.jpg')
         {
-            $photo = 'images/produtos/'.$product['caminhoimagem'].'.jpg';
+            $photo = 'images/produtos/'.$product['id'].'.jpg';
             updatePath($product['id'], $photo);
             $searchResult = getSearchResult($_GET['pesquisa'], $_GET['method_receiver']);
         }
