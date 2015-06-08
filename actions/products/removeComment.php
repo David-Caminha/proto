71a17a6d9e8dd87da670f2ba72c0022dbdc0032f
David-Caminha <david.caminha94@gmail.com>
2015-06-08 19:47:09 +0100

    ´remove

--- actions/products/removeComment.php
+++ actions/products/removeComment.php
@@ -7,6 +7,7 @@
 	$pp = populate_product_comment($_POST['idProduto']);
 
     $smarty->assign('Result', $pp);
+    $smarty->assign('idProd', $_POST['idProduto']);
     $output = $smarty->fetch('products/comentarios.tpl');
     echo $output;
 ?>
